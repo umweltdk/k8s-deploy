e98702d65350476d852e25e2c612234326fb4466
Brian Olsen <brian@maven-group.org>
2019-03-13 15:09:00 +0100

    Renamed nixos-18.09 tag to nixos-stable

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,5 +2,5 @@
 set -ev
 echo -n "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push umweltdk/k8s-deploy:latest
-docker push umweltdk/k8s-deploy:nixos-18.09
+docker push umweltdk/k8s-deploy:nixos-stable
 docker push umweltdk/k8s-deploy:nixos-unstable
